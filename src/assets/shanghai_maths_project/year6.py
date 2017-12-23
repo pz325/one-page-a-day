c20347cb8bbec6e655183f3ce110e2e0e8a651bb
pz325 <sg71.cherub@gmail.com>
2017-12-23 12:16:22 +0000

    add more exercises

--- src/assets/shanghai_maths_project/year6.py
+++ src/assets/shanghai_maths_project/year6.py
@@ -7,10 +7,17 @@ ZHENGLIN_YEAR6 = {
     'user': 'Zhenglin',
     'sections': [
         {
-            'title': '3.1 Using letters to represent numbers',
+            'title': '3.1 Using letters to represent numbers (1)',
             'exercies': [
                 r'$b+b+b+b\times b$ can be simply written as $\rule{2cm}{0.15mm}$',
                 r'In a triangle, if $\angle 1=a^{\circ}$ and $\angle 2=b^{\circ}$, then $\angle 3=$ $\rule{2cm}{0.15mm}$.',
+                r'In an isosceles triangle, if the base angle is $a^{\circ}$, the degree of the vertex angle is $\rule{2cm}{0.15mm}$',
+                r'When the sum of three consecutive even number is $a$, then the number in the middle is $\rule{2cm}{0.15mm}$, the least number is $\rule{2cm}{0.15mm}$ and the gratest number is $\rule{2cm}{0.15mm}$'
+            ]
+        }, # a section object
+        {
+            'title': '3.2 Using letters to represent numbers (2)',
+            'exercies': [
                 {
                     'description': r'Use expressions with letters to represent the relations between quantities.',
                     'exercises': [
@@ -18,9 +25,50 @@ ZHENGLIN_YEAR6 = {
                         r'$320$ is substracted by $12$ times $m$: $\rule{2cm}{0.15mm}$'
                     ]
                 },
-                r'In an isosceles triangle, if the base angle is $a^{\circ}$, the degree of the vertex angle is $\rule{2cm}{0.15mm}$',
-                r'When the sum of three consecutive even number is $a$, then the number in the middle is $\rule{2cm}{0.15mm}$, the least number is $\rule{2cm}{0.15mm}$ and the gratest number is $\rule{2cm}{0.15mm}$'
+                {
+                    'description': r'Use expressions with letters to represent the quantities below',
+                    'exercises': [
+                        r'A car has travelled $t$ hours at the speed of 85 km per hour. It has travelled $\rule{2cm}{0.15mm}$ $km$ in total',
+                        r'A shirt cost $a$ pounds, and a pair of trousers costs $b$ pounds.. The total cost of buying 3 sets of these clothes is $\rule{2cm}{0.15mm}$ pounds',
+                    ]
+                },
+                {
+                    'description': r'Look at each number sequence carefully and complete its $5^{th}$, $6^{th}$, and $n^{th}$ terms.',
+                    'exercises': [
+                        r'0, 5, 10, 15, $\rule{2cm}{0.15mm}$, $\rule{2cm}{0.15mm}$, ... , $\rule{2cm}{0.15mm}$',
+                        r'13, 23, 33, 43, $\rule{2cm}{0.15mm}$, $\rule{2cm}{0.15mm}$, ..., $\rule{2cm}{0.15mm}$',
+                    ]
+                },
             ]
-        } # a section object
+        }, # a section object
+                {
+            'title': '3.3 Simplification and evaluation (1)',
+            'exercies': [
+                {
+                    'description': r'Simplify the following expressions.',
+                    'exercises': [
+                        r'$36s-15t-24s+35t=$ $\rule{2cm}{0.15mm}$',
+                        r'$48x+75y-18x-6x$ $\rule{2cm}{0.15mm}$'
+                    ]
+                },
+                {
+                    'description': r'Fill in the blank',
+                    'exercises': [
+                        r'Joe has $x$ pencils. Roy has 3 more pencils than Joe. They have $\rule{2cm}{0.15mm}$ pencils altogether',
+                        r'Each pack of flour weights 10 kg. Each pack of rice weights $x$ kg. $y$ packs of flour and 5 packs of rice weight $\rule{2cm}{0.15mm}$ kg in total',
+                        r'Don, Evans and Frank each bought 4 pens at $a$ pounds each. They paid $\rule{2cm}{0.15mm}$ pounds in total for the pens. They also each bought $b$ exercise books. Each book costs 2 pounds. They paid $\rule{2cm}{0.15mm}$ pounds in total for the books.'
+                    ]
+                },
+                {
+                    'description': r'Short answer questions',
+                    'exercises': [
+                        r'\pounds10 can buy $3a$ kg of a fruit. Accordingly, Fiona bought $9.6a$ kg of the fruit with \pounds50. How much change did she get?',
+                        r'It took Joan $m$ hours to make 21 paper flowers. It took Marry 2 hours to make $n$ paper flowers. How many paper flowers did each of them make on average? How many paper flowers did both of them make every hour, on average?',
+                        r'The dividend is 6 times the divisor. If the divisor is $x$, what is the sum of the dividend, divisor, and quotient?'
+                    ]
+                },
+                r'The length and width of a rectangle are $a$ cm and $b$ cm respectively, and $a>b$. The side length of a square equals the difference between the two sides of the rectangle. What is the sum of their perimeters?'
+            ]
+        }, # a section object
     ]
 } # a book object
