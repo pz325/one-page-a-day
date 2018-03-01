5403dd1f1a37bc7921aeb89895b32ce8aa2cb8de
Ping Zou <zou@menrfa.home>
2018-03-01 15:11:30 +0000

    add Chinese year1

---
+++ src/assets/chinese/year1.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0301
+
+'''
+content for 语文 一年级 识字
+'''
+YU_WEN_SHIZI = {
+    'title': '语文 一年级 识字',
+    'user': 'Zhengyu',
+    'template': 'textbook',
+    'subject': 'chinese',
+    'sections': [
+        {
+            'title': '汉语拼音',
+            'exercises': [
+                ('ɑ', '阿姨'),
+                ('o', '我'),
+                ('e', '饿'),
+                ('i', '衣服'),
+                ('u', '屋子'),
+                ('ü', '鱼'),
+                ('yi', ''),
+                ('wu', ''),
+                ('yu', ''),
+                ('b', ''),
+                ('p', ''),
+                ('m', ''),
+                ('f', ''),
+                ('d', ''),
+                ('t', ''),
+                ('n', ''),
+                ('l', ''),
+                ('爸', 'bɑ̀'),
+                ('妈', 'mā'),
+                ('我', 'wǒ'),
+                ('大', 'dɑ̀'),
+                ('米', 'mǐ'),
+                ('土', 'tǔ'),
+                ('地', 'dì'),
+                ('马', 'mɑ̌'),
+            ]
+        },
+    ]
+}
+
+'''
+content for 语文 一年级 写字
+'''
+YU_WEN_XIEZI = {
+    'title': '语文 一年级 写字',
+    'user': 'Zhengyu',
+    'template': 'textbook',
+    'subject': 'chinese',
+    'sections': [
+        {
+            'title': '识字一',
+            'exercises': [
+                ('一', ''),
+                ('二', ''),
+                ('三', ''),
+            ]
+        },
+    ]
+}
--- src/export.py
+++ src/export.py
@@ -1,15 +1,24 @@
 import csv
-import assets.latin.year7
+import os
 import assets.spanish.year7
+import assets.latin.year7
+import assets.chinese.year1
 
 BOOKS = [
     assets.spanish.year7.ZOOM_ESPANOL_1,
     assets.latin.year7.ZHENGLIN_YEAR7,
-    assets.spanish.year7.SPELLING_BEE
+    assets.spanish.year7.SPELLING_BEE,
+    assets.chinese.year1.YU_WEN_SHIZI,
+    assets.chinese.year1.YU_WEN_XIEZI
 ]
 
+
+TARGET_PATH = './dist'
+if not os.path.exists(TARGET_PATH):
+    os.mkdir(TARGET_PATH)
+
 for book in BOOKS:
-    filename = '{user} {title}.csv'.format(user=book['user'], title=book['title'])
+    filename = '{target_path}/{user} {title}.csv'.format(target_path=TARGET_PATH, user=book['user'], title=book['title'])
     with open(filename, 'wb') as csv_file:
         writer = csv.writer(csv_file)
         for section in book['sections']:
