80ddf5e4741b5da4001bb35f4935ac4506738d91
Ping Zou <sg71.cherub@gmail.com>
2018-11-06 21:41:05 +0000

    add ISEB Spanish Vocabulary

--- src/export.py
+++ src/export.py
@@ -14,7 +14,8 @@ BOOKS = [
     assets.spanish.year7.TUTOR,
     assets.chinese.year1.YU_WEN_SHIZI,
     assets.chinese.year1.YU_WEN_XIEZI,
-    assets.spanish.year8.TUTOR
+    assets.spanish.year8.TUTOR,
+    assets.spanish.year8.ISEB_SPANISH_VOCABULARY
 ]
 
 
@@ -23,10 +24,10 @@ if not os.path.exists(TARGET_PATH):
     os.mkdir(TARGET_PATH)
 
 for book in BOOKS:
-    filename = '{target_path}/{user} {title}.csv'.format(target_path=TARGET_PATH, user=book['user'], title=book['title'])
+    filename = '{target_path}/{user} {title}.csv'.format(
+        target_path=TARGET_PATH, user=book['user'], title=book['title'])
     with open(filename, 'wb') as csv_file:
         writer = csv.writer(csv_file)
         for section in book['sections']:
             for exercise in section['exercises']:
                 writer.writerow(exercise)
-
